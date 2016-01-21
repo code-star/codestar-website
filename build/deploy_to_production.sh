b27f97c5a76c5ce46a1d7e5562f97e7b77fd0834
Maik Diepenbroek <maik.diepenbroek@ordina.nl>
2016-01-21 17:13:25 +0100

    Added image to deploy script

--- build/deploy_to_production.sh
+++ build/deploy_to_production.sh
@@ -5,8 +5,8 @@ git pull origin gh-pages
 git rm ./app.*
 cp dist/static/app.* ./
 cp dist/static/index.html ./
-cp dist/static/*.svg ./
-git add ./app.* ./index.html ./app/*.svg
+cp dist/static/logo.svg ./
+git add ./app.* ./index.html ./*.svg
 GIT_STATUS=$(git status 2> /dev/null)
 echo $GIT_STATUS | grep "nothing to commit" > /dev/null 2>&1
 if [ "$?" -ne 0 ]
