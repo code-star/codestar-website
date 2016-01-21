e68e45b481767ac5e65e4e7a0a64523591570028
Maik Diepenbroek <maik.diepenbroek@ordina.nl>
2016-01-21 15:19:27 +0100

    Hamza's ultra 1337 hacky deployment

--- build/deploy_to_production.sh
+++ build/deploy_to_production.sh
@@ -6,7 +6,12 @@ git rm ./app.*
 cp dist/static/app.* ./
 cp dist/static/index.html ./
 git add ./app.* ./index.html
-now=$(date "+%d-%m-%Y %H:%M:%S")
-git commit -m "Updated website on: $now"
-git push origin gh-pages
+local GIT_STATUS=$(git status 2> /dev/null)
+echo $GIT_STATUS | grep "nothing to commit" > /dev/null 2>&1
+if [ "$?" -ne 0 ]
+then
+  now=$(date "+%d-%m-%Y %H:%M:%S")
+  git commit -m "Updated website on: $now"
+  git push origin gh-pages
+fi
 git checkout master
