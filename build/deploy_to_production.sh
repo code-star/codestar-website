6a582c23e28deef099dc446d6db03a19736df8f0
Maik Diepenbroek <maik.diepenbroek@ordina.nl>
2016-01-21 15:03:53 +0100

    Another improvement

--- build/deploy_to_production.sh
+++ build/deploy_to_production.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-git pull
 npm run deploy
 git checkout gh-pages
+git pull
 git rm ./app.*
 cp dist/static/app.* ./
 cp dist/static/index.html ./
-git add app.* index.html
+git add ./app.* ./index.html
 now=$(date "+%d-%m-%Y %H:%M:%S")
 git commit -m "Updated website on: $now"
 git push origin gh-pages
