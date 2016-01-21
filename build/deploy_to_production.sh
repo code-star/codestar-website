fb3376f70feacb93d705d3fd5b1c778680844dfd
Maik Diepenbroek <maik.diepenbroek@ordina.nl>
2016-01-21 17:06:39 +0100

    Fixed image loading

--- build/deploy_to_production.sh
+++ build/deploy_to_production.sh
@@ -5,7 +5,8 @@ git pull origin gh-pages
 git rm ./app.*
 cp dist/static/app.* ./
 cp dist/static/index.html ./
-git add ./app.* ./index.html
+cp dist/static/*.svg ./
+git add ./app.* ./index.html ./app/*.svg
 GIT_STATUS=$(git status 2> /dev/null)
 echo $GIT_STATUS | grep "nothing to commit" > /dev/null 2>&1
 if [ "$?" -ne 0 ]
