4dc2a8c4b7e0198d044ee30969a4b694c4b81c5f
Maik Diepenbroek <maik.diepenbroek@ordina.nl>
2016-01-21 14:51:01 +0100

    Improved deploy script

---
+++ build/deploy_to_production.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git pull
+npm run deploy
+git checkout gh-pages
+git rm app.*
+cp dist/static/app.* ./
+cp dist/static/index.html ./
+git add app.* index.html
+now=$(date "+%d-%m-%Y %H:%M:%S")
+git commit -m "Updated website on: $now"
+git push origin gh-pages
+git checkout master
