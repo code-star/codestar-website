7b820365c39860299723fc74231d4be17254c022
Hamza Haiken <hamza.haiken@ordina.nl>
2016-01-22 09:50:06 +0100

    add d3 sunburst behind logo

--- app/main.js
+++ app/main.js
@@ -8,6 +8,8 @@ require('./fonts/ConduitITCStd-BoldItalic.otf');
 require('./fonts/ConduitITCStd-Italic.otf');
 
 import $ from 'jquery';
+import d3 from 'd3';
+import { appendSunburst } from './js/sunburst';
 let Scrollax = require('scrollax');
 let parallax = new Scrollax(window, {'horizontal': true}).init();
 let mouseWheel = require('jquery-mousewheel');
@@ -15,6 +17,7 @@ let mouseWheel = require('jquery-mousewheel');
 $(document).ready(function(){
   var offset = $('#center').position().left;
   window.scrollTo(offset, 0);
+  appendSunburst('#sunburst');
   console.log($('#logo').find('#paren1').attr('d'));
   console.log($('#paren1').attr('d'));
 });
