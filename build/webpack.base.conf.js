359a9cbaa87605169712ffb8862b197390cfa030
Hamza Haiken <hamza.haiken@ordina.nl>
2016-01-29 14:50:37 +0100

    fix the build script... again!

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist/static'),
-    publicPath: '/',
+    publicPath: './',
     filename: '[name].js'
   },
   resolve: {
