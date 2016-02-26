c4bba04f7f594db3e02755dcc8368fd7b4506c58
Maik Diepenbroek <maik.diepenbroek@ordina.nl>
2016-02-26 14:15:55 +0100

    Made the dynamic require work

--- app/js/dynamicRequire.js
+++ app/js/dynamicRequire.js
@@ -1,4 +1,5 @@
-var assets = require.context("../", true, /(?!.*[.](?:png|svg|otf|woff)$).*/igm);
+let assets = require.context("../", true, /^.*(.png|.svg|.otf|.woff)$/);
 assets.keys().forEach(function(key){
+  console.log(key);
     assets(key);
 });
