0b1783c04457edf58938a99885edf4258cc622b1
M.D. van Es <es@mdworld.nl>
2017-05-06 19:32:56 +0200

    Added basic unit test to prevent npm run test from failing.

---
+++ test/main.spec.js
@@ -0,0 +1,11 @@
+/* eslint-env jasmine */
+
+import { JobList } from '../app/js/JobList';
+
+describe('JobList', function() {
+
+    it('should be defined', function() {
+        expect(JobList).toBeDefined();
+    })
+
+});
