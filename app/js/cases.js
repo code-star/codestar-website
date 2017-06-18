f97fef0efb83ab45e5b2963fc8a5eeb11f84fbf6
M.D. van Es <es@mdworld.nl>
2017-06-18 16:23:45 +0200

    #19 some refactoring of Cases to make it easier to add new functionality

--- app/js/cases.js
+++ app/js/cases.js
@@ -1,44 +1,45 @@
-var cases = require('../data/cases.json');
+const cases = require('../data/cases.json');
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-export function getCasesTree() {
-  var svg = d3.select('body').append('svg').remove();
+class CasesTree {
+  constructor() {
+    var svg = d3.select('body').append('svg').remove();
 
-  var width = 800,
+    var width = 800,
       height = 300;
 
-  var force = d3.layout.force()
-    .charge(-1000)
-    .linkDistance(() => getRandomInt(200, 300))
-    .size([width, height]);
+    var force = d3.layout.force()
+      .charge(-1000)
+      .linkDistance(() => getRandomInt(200, 300))
+      .size([width, height]);
 
-  svg
-    .attr('width', width)
-    .attr('height', height);
+    svg
+      .attr('width', width)
+      .attr('height', height);
 
-  var size = cases.nodes.length;
-  var links = [];
-  for (var i = 0; i < size; ++i) {
-    var n_links = getRandomInt(1, 2);
-    for (var j = 0; j < n_links; ++j) {
-      var target = getRandomInt(0, size);
-      while (target === i) {
-        target = getRandomInt(0, size);
+    var size = cases.nodes.length;
+    var links = [];
+    for (var i = 0; i < size; ++i) {
+      var n_links = getRandomInt(1, 2);
+      for (var j = 0; j < n_links; ++j) {
+        var target = getRandomInt(0, size);
+        while (target === i) {
+          target = getRandomInt(0, size);
+        }
+        links.push({'source': i, 'target': target});
       }
-      links.push({'source': i, 'target': target});
     }
-  }
 
-  force
+    force
       .nodes(cases.nodes)
       .links(links)
       .start();
 
-  svg
-  .append('defs')
+    svg
+      .append('defs')
       .selectAll('.node')
       .data(cases.nodes).enter()
       .append('pattern')
@@ -48,21 +49,21 @@ export function getCasesTree() {
       .attr('height', '100%')
       .attr('width', '100%')
       .attr('viewBox', '0 0 120 120')
-          .append('image')
-          .attr('xlink:href', function(c) {return require('../img/Cases/' + c.icon)})
-          .attr('x', 0)
-          .attr('y', 0)
-          .attr('width', 120)
-          .attr('height', 120)
-          /* IE 10 Doesn't support SVG filters and breaks the graphic if used. Curiously this doesn't happen in IE9 */
-          .style('filter', (navigator.appVersion.indexOf('MSIE 10') === -1)?'url(#desaturate)':'');
+      .append('image')
+      .attr('xlink:href', function(c) {return require('../img/Cases/' + c.icon)})
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', 120)
+      .attr('height', 120)
+      /* IE 10 Doesn't support SVG filters and breaks the graphic if used. Curiously this doesn't happen in IE9 */
+      .style('filter', (navigator.appVersion.indexOf('MSIE 10') === -1)?'url(#desaturate)':'');
 
-  var link = svg.selectAll('.link')
+    var link = svg.selectAll('.link')
       .data(links)
       .enter().append('line')
       .attr('class', 'case-link');
 
-  var node = svg.selectAll('.node')
+    var node = svg.selectAll('.node')
       .data(cases.nodes)
       .enter().append('circle')
       .attr('class', 'case-node')
@@ -71,17 +72,18 @@ export function getCasesTree() {
       .style('fill', function(c) {return 'url(#icon' + c.icon + ')'})
       .call(force.drag);
 
-  node.append('title')
+    node.append('title')
       .text(function(d) { return d.company; });
 
-  node.on('mousedown', function(d) {
-    var n = d3.select(this);
-    // Capture starting position
-    n.attr('data-startcx', n.attr('cx'))
-    n.attr('data-startcy', n.attr('cy'))
-  })
+    node.on('mousedown', function(d) {
+      var n = d3.select(this);
+      // Capture starting position
+      n.attr('data-startcx', n.attr('cx'))
+      n.attr('data-startcy', n.attr('cy'))
+    })
 
-  node.on('click', function(d) {
+    const self = this;
+    node.on('click', function(d) {
       var n = d3.select(this);
 
       // Check if the user is clicking or moving the node
@@ -101,39 +103,64 @@ export function getCasesTree() {
 
       // Toggle the node
       if (!n.attr('old_r')) {
-        node.each(function (d, i) {
-          var m = d3.select(this);
-
-          var old_r = m.attr('old_r');
-          if (old_r) m.attr('r', old_r);
-          m.attr('old_r', null);
-        });
-
-        n.attr('old_r', n.attr('r'));
-        n.attr('r', 61);
-
-        $('#caseName').html(d.company + '<br><span style="font-weight: normal">' + d.project + '</span>');
-        $('#caseDesc').html(d.description);
-
-        $('#caseInfo').slideDown(400)
+        self.openCaseInfo(node, n, d)
       } else {
-        n.attr('r', n.attr('old_r'));
-        n.attr('old_r', null);
-
-        $('#caseInfo').slideUp(400)
+        self.closeCaseInfo(n);
       }
 
-  });
+    });
 
-  force.on('tick', function() {
-    node.attr('cx', function(d) { return d.x = Math.max(60, Math.min(width - 60, d.x)); })
+    force.on('tick', function() {
+      node.attr('cx', function(d) { return d.x = Math.max(60, Math.min(width - 60, d.x)); })
         .attr('cy', function(d) { return d.y = Math.max(60, Math.min(height - 60, d.y)); });
 
-    link.attr('x1', function(d) { return d.source.x; })
+      link.attr('x1', function(d) { return d.source.x; })
         .attr('y1', function(d) { return d.source.y; })
         .attr('x2', function(d) { return d.target.x; })
         .attr('y2', function(d) { return d.target.y; });
-  });
+    });
 
-  return {'svg': svg.node(), 'layout': force};
+    this.svg = svg;
+    this.force = force;
+  }
+
+  getCasesTree() {
+    return {
+      svg: this.svg.node(),
+      layout: this.force
+    }
+  }
+
+  openCaseInfo(node, n, clickedNode) {
+    node.each(function() {
+      var m = d3.select(this);
+
+      var old_r = m.attr('old_r');
+      if (old_r) {
+        m.attr('r', old_r);
+      }
+      m.attr('old_r', null);
+    });
+
+    n.attr('old_r', n.attr('r'));
+    n.attr('r', 61);
+
+    $('#caseName').html(clickedNode.company + '<br><span style="font-weight: normal">' + clickedNode.project + '</span>');
+    $('#caseDesc').html(clickedNode.description);
+
+    $('#caseInfo').slideDown(400)
+  }
+
+  closeCaseInfo(n) {
+    n.attr('r', n.attr('old_r'));
+    n.attr('old_r', null);
+
+    $('#caseInfo').slideUp(400);
+  }
+
+}
+
+export function getCasesTree(){
+  const casesTree = new CasesTree();
+  return casesTree.getCasesTree();
 }
