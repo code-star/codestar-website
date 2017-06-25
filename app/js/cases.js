a4dfe48d4f1679ffb4410306f4882cb8f87967a2
M.D. van Es <es@mdworld.nl>
2017-06-25 11:00:46 +0200

    #19 Reverting SlideOutArticle in cases

--- app/js/cases.js
+++ app/js/cases.js
@@ -1,5 +1,3 @@
-import SlideOutArticle from './SlideOutArticle';
-
 const cases = require('../data/cases.json');
 
 function getRandomInt(min, max) {
@@ -11,8 +9,6 @@ class CasesTree {
     this.$caseName =$('#caseName');
     this.$caseDesc = $('#caseDesc');
     this.$caseInfo = $('#caseInfo');
-    this.slideOutArticle = new SlideOutArticle($('#caseName').closest('section').find('.row').first());
-    $('#caseDesc').on('click', '.toggleSlideOutArticle', ev => this.slideOutArticle.toggle());
 
     const svg = d3.select('body').append('svg').remove();
 
