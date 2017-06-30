0a8c54f15ae601d86da97d56f185ffbed1c2febf
Bjorn S <bjorn.schijff@ordina.nl>
2017-06-30 11:07:10 +0200

    Fixes contactform.

--- app/js/ContactForm.js
+++ app/js/ContactForm.js
@@ -1,25 +1,34 @@
 import { CONTACT_EMAIL_ADDRESS } from './constants';
 export class ContactForm {
+
   constructor() {
     $('#contact_form').foundation();
+    console.log(this);
   }
 
   bindValidationToForm() {
+    console.log($().jquery);
+    console.log('We gaan enorm binden.');
+    console.log(this);
+    var that = this;
     $('#contact_form')
-    .bind('submit', function(e) {
-      e.preventDefault();
-    })
-    .bind('formvalid.zf.abide', function(e,$form) {
-      let serializedForm = $form.serializeArray();
-      let messageData = {};
-      serializedForm.map(field => {
-        messageData[field.name] = field.value;
+      .on('submit', function(e) {
+        console.log(e);
+        e.preventDefault();
+      })
+      .on('formvalid.zf.abide', function(e,$form) {
+        e.preventDefault();
+        let messageData = that.createMessageData($form)
+        that.submitForm(messageData);
+      })
+      .on('forminvalid.zf.abide', function(e, $form) {
+        console.log(e);
+        console.log($form);
+        let messageData = this.createMessageData($form)
       });
-
-      that.submitForm();
-    });
   }
-  submitForm() {
+
+  submitForm(messageData) {
     $.ajax({
       url: '//formspree.io/'+CONTACT_EMAIL_ADDRESS,
       method: 'POST',
@@ -32,6 +41,7 @@ export class ContactForm {
       $('.email-fail').toggle('slow');
     });
   }
+
   addClickListeners() {
     $('.email-success .close-button').click(() => {
       $('.email-success').toggle('slow');
@@ -39,5 +49,14 @@ export class ContactForm {
     $('.email-fail .close-button').click(() => {
       $('.email-fail').toggle('slow');
     });
+  }
+
+  createMessageData($form) {
+    let serializedForm = $form.serializeArray();
+    let messageData = {};
+    serializedForm.map(field => {
+      messageData[field.name] = field.value;
+    });
+    return messageData;
   }
 }
